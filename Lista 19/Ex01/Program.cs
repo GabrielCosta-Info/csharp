05bcdc05de8efca4d7696c51b0bb6c2330aa8390
Gabriel Lima <gabrielcostalima21@hotmail.com>
2019-10-25 18:21:23 -0300

    terminado e finalizado

---
+++ Lista 19/Ex01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+            do
+            {
+                Console.WriteLine("1: Quadrado.\n2: Retângulo.\n0: Sair.");
+                opcao = int.Parse(Console.ReadLine());
+                if (opcao == 1)
+                {
+                    Console.WriteLine("Digite lado do quadrado");
+                    Quadrado quadrado = new Quadrado(double.Parse(Console.ReadLine()));
+                    Console.WriteLine(quadrado);
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+                if (opcao == 2)
+                {
+                    Console.WriteLine("Base do retângulo.");
+                    double basee = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Altura do retângulo.");
+                    double altura = double.Parse(Console.ReadLine());
+                    Retangulo retangulo = new Retangulo(basee, altura);
+                    Console.WriteLine(retangulo);
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+            } while (opcao != 0);
+        }
+    }
+}
